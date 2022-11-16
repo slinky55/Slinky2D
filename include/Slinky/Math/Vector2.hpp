8c8e46b1658cd6119a556723f7364576ad8d32f5
slinky55 <larry.mason@ufl.edu>
2022-11-16 00:11:56 -0500

    Finished base Vector2 class

--- include/Slinky/Math/Vector2.hpp
+++ include/Slinky/Math/Vector2.hpp
@@ -3,14 +3,15 @@ namespace slinky::math
 {   
     struct Vector2
     {
-        float x;
-        float y;
+        float x {0};
+        float y {0};
 
         Vector2() = default;
+        ~Vector2() = default;
         Vector2(float _x,
                 float _y);
         Vector2(const Vector2& _other);
-        Vector2& operator=(const Vector2& _rhs);
+        Vector2& operator=(const Vector2& _rhs) = default;
     };
 
 }
